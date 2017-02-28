ad66a51e5e58af73c977e13c22a0321f97463494
jozephhh <joseph.q@me.com>
2017-02-28 20:10:55 +0100

    :sparkles: Safe login and errors detections
    Signed-off-by: jozephhh <joseph.q@me.com>

---
+++ login.php
@@ -0,0 +1,76 @@
+
+
+<?
+
+include "components/db.php";
+
+$errors_list = array();
+if (!empty($_POST)) {
+    $login = trim($_POST["mail"]);
+    $password = trim($_POST["password"]);
+
+    if(empty($login)) {
+        $errors_list["mail"] = "missing value";
+    }
+    if(empty($password)) {
+        $errors_list["password"] = "missing value";
+    }
+    else if(empty($errors_list)){
+        define("SALT","foo");
+        $password =hash("sha256", SALT.$password);
+        $prepare = $pdo->prepare('SELECT * FROM users WHERE email = :email LIMIT 1');
+        $prepare->bindValue('email',$login);
+        $prepare->execute();
+        $user = $prepare->fetch();
+
+// Test password
+if(gettype($user) ==="object" && $user->password == $password)
+    echo 'You shall pass !';
+else
+    $errors_list["password_error"] = "login error";
+
+}
+}
+else {
+    $_POST["mail"]="";
+    $_POST["password"]="";
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Login - Inventory</title>
+</head>
+<body>
+    <div class="background"></div>
+    <div class="background_color"></div>
+
+    <div class="container">
+        <div class="logs">
+            <?
+            echo '<pre>';
+            print_r($errors_list);
+            echo '</pre>';
+            ?>
+        </div>
+        <form class="form" action="login.php" method="post">
+            <i class="fa fa-sliders" aria-hidden="true"></i>
+            <h3>Inventory gestion login</h3>
+            <div class="form_row">
+                <label for="mail">E-mail <span><?= array_key_exists('mail', $errors_list) ? '- Missing value' : ''  ?></span><span><?= array_key_exists('password_error', $errors_list) ? "- Erreur d'indentification" : ''  ?></span></label>
+                <input type="email" name="mail" value="<?= $_POST["mail"] ?>" id="mail" placeholder="john.doe@gmail.com">
+            </div>
+            <div class="form_row">
+                <label for="password">Password <span><?= array_key_exists('password', $errors_list) ? '- Missing value' : ''  ?></span><span><?= array_key_exists('password_error', $errors_list) ? "- Erreur d'indentification" : ''  ?></span></label>
+                <input type="password" name="password" value="" id="password" placeholder="Only you know it.">
+            </div>
+            <input type="submit" name="" value="Login" class="form_submit">
+        </form>
+    </div>
+</body>
+</html>
---
+++ store.php
@@ -0,0 +1,17 @@
+<?php
+    include "components/db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Store</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        
+    </div>
+</body>
+</html>
